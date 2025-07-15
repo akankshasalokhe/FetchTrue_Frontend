d0f1f8317c2afe2ddfd324b1cf0ea41b04a1e3b9
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-07-15 17:35:41 +0530

    update

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FaWhatsapp } from "react-icons/fa";
-import './WhatsappButton.css'
+import './WhatsappButton.css';
 
 const WhatsAppButton = () => {
   const [scrolled, setScrolled] = useState(false);
